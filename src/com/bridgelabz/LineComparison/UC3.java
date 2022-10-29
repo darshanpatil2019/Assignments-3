71e3e5b4172df533947401937ba981cbba855730
Darshan Patil <iampatildarsh>
2022-10-29 18:42:12 +0530

    functionality to knw

---
+++ src/com/bridgelabz/LineComparison/UC3.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.LineComparison;
+
+import java.util.*;
+import java.lang.*;
+
+public class UC3 {
+    public static void main(String[] args) {
+        // Object creation for Scanner class
+        Scanner sc = new Scanner(System.in);
+            /*As a fan of geometry, I want to compare two lines based on the end points, So that I know one line is equal,
+             greater or less than the other line. - Using Java compareTo method to compare 2 Lengths is preferable*/
+
+        double length[] = new double[2];
+        // user inputs
+        for (int i = 0; i < 2; i++) {
+
+            System.out.println((i + 1) + ":Enter the values for x1,y1,x2,y2");
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            // calculate length of line and store in array
+            length[i] = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            System.out.println("Length of line" + (i + 1) + ":" + length[i]);
+        }
+        // checking equality of 2 lines - comparing lengths of two lines
+        if (Objects.equals(length[0], length[1])) {
+            System.out.println("The length of two lines are equal");
+        } else if (length[0] > length[1]) {
+            System.out.println("The Length of line 0 is greater than the length of line 1");
+        } else {
+            System.out.println("Length of line 1 is greater than the length of line 0");
+
+        }
+    }
+}
+
+
